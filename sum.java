4014a9e60ccf07b7842bd9854a1ff0dfb44cb278
Shreya.S <tss.shreya@gmail.com>
2022-11-19 19:52:09 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sum {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number ");
+        int num1=sc.nextInt();
+        System.out.println("enter another number");
+        int num2=sc.nextInt();
+        int sum=0;
+        sum=num1+num2;
+        System.out.println("sum="+sum);
+
+
+    }
+    
+}
